45169ad2343c4f52cbef789225c34efcbce57fe4
Kein2408 <kein2408@gmail.com>
2019-10-12 17:12:56 +0300

    stage 2

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -2,13 +2,40 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'head_title': 'главная',
+    }
+    return render(request, 'mainapp/index.html', context)
 
 
 def catalog(request):
-    return render(request, 'mainapp/catalog.html')
+    catalog = [
+        {
+            'item_name': 'Мэтт Вайсфельд. Объектно-ориентированное мышление',
+            'item_link': '/catalog/card1.html',
+            'item_img_link': 'img/book1.jpg',
+        },
+        {
+            'item_name': 'Бертран Мейер. Почувствуй класс',
+            'item_link': '/catalog/card2.html',
+            'item_img_link': 'img/book2.jpg',
+        },
+        {
+            'item_name': 'Гради Буч. Объектно-ориентированный анализ и проектирование с примерами приложений',
+            'item_link': '/catalog/card3.html',
+            'item_img_link': 'img/book3.jpg',
+        },
+    ]
+    context = {
+        'head_title': 'каталог',
+        'catalog': catalog,
+    }
+    return render(request, 'mainapp/catalog.html', context)
 
 
 def contact(request):
-    return render(request, 'mainapp/contacts.html')
+    context = {
+        'head_title': 'контакты',
+    }
+    return render(request, 'mainapp/contacts.html', context)
 # Create your views here.
